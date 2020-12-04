baad2000c22d4caab066b95c4e1fc59b4df4b7ea
Raphael Almeida <raphael.almeida@ip-192-168-15-21.ec2.internal>
2020-12-03 22:00:19 -0300

    Add roman numerals challenge

---
+++ roman-numerals/lib/roman_numerals.ex
@@ -0,0 +1,10 @@
+defmodule RomanNumerals do
+  @doc """
+  Convert the number to a roman number.
+  """
+  @spec numeral(pos_integer) :: String.t()
+  def numeral(number) do
+    numeral_map = %{1 => "I"}
+    numeral_map[number]
+  end
+end
